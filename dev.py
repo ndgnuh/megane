ae884f425d9f41428180d9e0511e8a4a54cc05f4
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-06 16:09:25 +0700

    more training stuff

--- dev.py
+++ dev.py
@@ -10,6 +10,7 @@ from eyeball import processor
 from eyeball.tools import meanap, map as MaP
 from torch import optim, nn
 from tqdm import tqdm
+from argparse import Namespace
 icecream.install()
 
 
@@ -37,31 +38,38 @@ class LossFunction(nn.Module):
 class Trainer(LightningLite):
     def __init__(self, config, **k):
         super().__init__(**k)
+        self.batch_size = config.batch_size
+        self.mode = config.mode
         self.model = detector.Detector(config)
         self.processor = processor.DBProcessor()
         self.loss = LossFunction(config.mode)
         self.optimizer = optim.AdamW(self.model.parameters(),
                                      config.learning_rate)
-        self.batch_size = 8
         self.train_loader = self.mk_dataloader('data')
         self.val_loader = self.mk_dataloader('data')
-        self.valid_every = 100
-        self.num_steps = 1000
+        self.validate_every = 200
+        self.print_every = 200
+        self.num_steps = 100000
 
     @torch.no_grad()
     def run_validate(self, model):
-        model.eval()
         val_loader = self.setup_dataloaders(self.val_loader)
         score = []
+        losses = []
         for image, annotations in tqdm(val_loader, "Validating"):
             outputs = model(image)
+            loss = self.loss(outputs, annotations)
             prs = self.processor.post(outputs)
             gts = self.processor.post(annotations)
             meanap = MaP.calc_mean_ap(prs, gts)
             score.append(meanap)
+            losses.append(loss)
         score = sum(score) / len(score)
-        ic(score)
-        return {}
+        loss = sum(losses) / len(losses)
+        return dict(meanap=score, loss=loss)
+
+    def is_matching_step(self, perstep):
+        return self.global_step % perstep == 0 and self.global_step > 0
 
     def run(self):
         model, optimizer = self.setup(self.model, self.optimizer)
@@ -86,23 +94,43 @@ class Trainer(LightningLite):
             self.backward(loss)
             optimizer.step()
 
+            if self.is_matching_step(self.print_every):
+                info = "[Train] step: {step}/{total} - loss {loss:.4f}"
+                info = info.format_map({
+                    "step": self.global_step,
+                    "total": self.num_steps,
+                    "loss": loss.item()
+                })
+                tqdm.write(info)
+
             # validation
-            if self.global_step % self.valid_every == 0 and self.global_step > 0:
-                val_metrics = self.run_validate(model)
+            if self.is_matching_step(self.validate_every):
+                metrics = self.run_validate(model)
+                info = "[Validate] step: {step}/{total} - loss: {loss:.4f} - MeanAP: {meanap:.2f}"
+                metrics['step'] = self.global_step
+                metrics['total'] = self.num_steps
+                info = info.format_map(metrics)
+                tqdm.write(info)
 
     def mk_dataloader(self, data_root):
         data = EyeballDataset(
-            data_root, 640, 640,
+            data_root, 224, 224,
             preprocess=self.processor.pre
         )
         return DataLoader(data, batch_size=self.batch_size)
 
 
-config = detector.Config(
+class Predictor:
+    def __init__(self, image_width: int, image_height: int):
+        pass
+
+
+config = Namespace(
     backbone="fpn_resnet18",
     mode="db",
     feature_size=256,
-    learning_rate=8e-4
+    learning_rate=3e-4,
+    batch_size=4,
 )
 
 # x = torch.rand(1, 3, 1024, 1024)
