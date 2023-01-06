8d282e37c02cd9cce32372fddab4e76857c61983
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-06 09:57:28 +0700

    hack

---
+++ eyeball/tools/remember.py
@@ -0,0 +1,16 @@
+from functools import wraps
+import sys
+import inspect
+
+
+def remeber(init_method):
+    @wraps(init_method)
+    def wrap_init(self, *arg, **kwargs):
+        allkwargs = inspect.getcallargs(init_method, self, *arg, **kwargs)
+        for k, v in allkwargs.items():
+            setattr(self, k, v)
+        init_method(self, *arg, **kwargs)
+    return wrap_init
+
+
+sys.modules[__name__] = remeber
