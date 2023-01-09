83f45c0e5364c241b96ef8ba1bfb8c6b03633f07
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-01-10 00:40:43 +0700

    more augment

--- eyeball/aug.py
+++ eyeball/aug.py
@@ -1,10 +1,25 @@
-from PIL import Image, ImageOps, ImageColor, ImageEnhance
+from PIL import Image, ImageOps, ImageColor, ImageEnhance, ImageChops
 import random
 import numpy as np
 
 
 def rand(a, b, step):
     return random.choice(np.arange(a, b+step, step=step))
+
+
+def gaussian_noise(image, level):
+    noise = Image.effect_noise(image.size, 255).convert("RGB")
+    return ImageChops.blend(image, noise, level)
+
+
+def salt_and_pepper(image, prob):
+    w, h = image.size
+    mask = np.random.rand(h, w, 1) <= prob
+    pors = (np.random.rand(h, w, 1) < 0.5) * 255
+    image = np.array(image)
+    image = pors * mask + (1 - mask) * image
+    image = image.round().astype('uint8')
+    return Image.fromarray(image)
 
 
 def sometime(f, p=0.3):
@@ -56,5 +71,9 @@ default_augment = random_apply([
     oneof([
         randomized(brightness, np.arange(0.8, 1.21, step=0.01)),
         randomized(contrast, np.arange(0.8, 1.21, step=0.01))
+    ]),
+    oneof([
+        randomized(gaussian_noise, np.arange(0.05, 0.3, step=0.01)),
+        randomized(salt_and_pepper, np.arange(0.05, 0.15, step=0.01)),
     ])
 ], 0.3)
