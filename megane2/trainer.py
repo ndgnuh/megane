8c1c63e1313bab13ef2f07593f4a7508f47306fd
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-19 15:02:11 +0700

    [wip] saving weights

--- megane2/trainer.py
+++ megane2/trainer.py
@@ -7,6 +7,7 @@ from torch import optim
 from tqdm import tqdm
 from . import visualize
 import random
+from . import stats
 
 
 def cycle(dataloader, total_steps):
@@ -82,6 +83,10 @@ class Trainer(LightningLite):
         print_every = validate_every // 5
         criterion = self.criterion
 
+        # Metrics
+        avg_train_loss = stats.AverageStatistic()
+        max_score = stats.MaxStatistic()
+
         # Training with steps
         pbar = cycle(train_loader, total_steps)
         pbar = tqdm(pbar, "Training", total=total_steps, dynamic_ncols=True)
@@ -95,19 +100,26 @@ class Trainer(LightningLite):
             optimizer.step()
             lr_scheduler.step()
 
+            avg_train_loss.append(loss.item())
             if step % print_every == 0:
                 lr = optimizer.param_groups[0]['lr']
+
                 info = [
                     f"Step: {step}/{total_steps}",
-                    f"Loss: {loss.item():.4f}",
+                    f"Loss: {avg_train_loss.summarize():.4f}",
+                    f"Best: {max_score.summarize():.3f}",
                     f"Lr: {lr:.3e}",
                 ]
                 tqdm.write(" - ".join(info))
 
             if step % validate_every == 0:  # Step are 1 based
                 metrics = self.validate()
+                changed = max_score.append(metrics['score'])
                 info = [f"{k}: {v}" for k, v in metrics.items()]
                 tqdm.write(" - ".join(info))
+                if changed:
+                    torch.save(model.state_dict(), "storage/weights/best.pt")
+                    tqdm.write("Best model saved to storage/weights/best.pt")
 
     @torch.no_grad()
     def validate(self):
@@ -118,10 +130,14 @@ class Trainer(LightningLite):
         num_batches = len(val_loader)
         visualize_index = random.choice(range(num_batches))
 
+        avg_f1_score = stats.AverageStatistic()
+        avg_val_loss = stats.AverageStatistic()
+
         count = 0
-        for images, annotations in val_loader:
+        for images, annotations in tqdm(val_loader, "Validating", dynamic_ncols=True):
             outputs = model(images)
             loss = criterion(*outputs, *annotations)
+            avg_val_loss.append(loss.item())
 
             # Check accuracy
             # The first map is the probability map
@@ -134,21 +150,19 @@ class Trainer(LightningLite):
                 target_polygons, _, _ = self.post_processor(
                     target_proba_map.cpu().numpy().astype('float32')
                 )
-                print(polygons, target_polygons)
-                score = scores.f1_score(polygons, target_polygons)
+                avg_f1_score.append(scores.f1_score(polygons, target_polygons))
 
             if visualize_index == count:
                 proba_map = proba_maps[0][0].cpu().numpy()
                 target_proba_map = torch.sigmoid(
                     proba_maps[0][0]).cpu().numpy()
                 cv2.imshow("proba_map", proba_map)
-                import numpy as np
-                np.save("proba_map.npy", proba_map)
-                np.save("proba_map.npy", proba_map)
-                # cv2.imwrite("proba_map.jpg", proba_map)
                 cv2.waitKey(25)
 
             count = count + 1
 
         # THIS IS A DRAFT
-        return dict(f1score=score)
+        return dict(
+            score=avg_f1_score.summarize(),
+            loss=avg_val_loss.summarize()
+        )
