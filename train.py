1add5f2e89092b537b56cf60af326ad3f97bdf69
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-30 10:27:46 +0700

    interactive train.py

--- train.py
+++ train.py
@@ -1,13 +1,33 @@
-from megane import Trainer, read_yaml
-from argparse import ArgumentParser
+import questionary as Q
+from os import path
+from megane2.trainer import Trainer
+from megane2.configs import read_config
 
-parser = ArgumentParser()
-parser.add_argument("--model-config", "-c",
-                    dest="model_config",
-                    required=True, )
-args = parser.parse_args()
 
-train_config = read_yaml("configs/train.yaml")
-model_config = read_yaml(args.model_config)
-trainer = Trainer(model_config, train_config)
-trainer.run()
+def ask_int(prompt, default):
+    q = Q.text(prompt, validate=str.isnumeric, default=str(default))
+    return int(q.ask())
+
+
+def ask_file(prompt, default=""):
+    q = Q.path(prompt, validate=path.isfile, default=str(default))
+    return q.ask()
+
+
+def main():
+    options = dict()
+    options["model_config"] = read_config(ask_file("Model config (.yml):"))
+    options["train_data"] = ask_file("Train data index:")
+    train_data_dir = path.dirname(options["train_data"])
+    options["val_data"] = ask_file("Validate data index:", train_data_dir)
+    options["batch_size"] = ask_int("Batch size:", 4)
+    options["num_workers"] = ask_int("Number of workers", 1)
+    options["total_steps"] = ask_int("Total training steps:", 1000)
+    options["validate_every"] = ask_int("Validate every:", 50)
+
+    trainer = Trainer(**options)
+    trainer.run()
+
+
+if __name__ == "__main__":
+    main()
