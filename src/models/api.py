97947891cc15ff503b99d238db2dbb4cea31e549
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-12 12:03:05 +0700

    Revert "model api: enforce collate_fn implementation"
    This reverts commit 3680753307a2b43a4269738a04f17ed9939cfb51.

--- src/models/api.py
+++ src/models/api.py
@@ -1,6 +1,7 @@
 from typing import TypeVar
 from abc import ABC, abstractmethod
 from torch import nn, Tensor
+from torch.utils.data import default_collate
 from ..data import Sample
 
 T = TypeVar("T")
@@ -25,6 +26,5 @@ class ModelAPI(nn.Module, ABC):
     def visualize_outputs(self, outputs, grouth_truth: bool = False) -> Tensor:
         ...
 
-    @abstractmethod
     def collate_fn(self, samples):
-        ...
+        return default_collate(samples)
