177b71b5ca05547b13867d8f4b4220af18e7ae61
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-03 15:46:14 +0700

    debug utils with timer

---
+++ megane/debug.py
@@ -0,0 +1,23 @@
+import sys
+import logging
+import time
+from functools import wraps
+
+
+logger = logging.getLogger("megane-debug")
+handler = logging.StreamHandler(sys.stdout)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logger)
+formatter = logging.Formatter()
+
+
+def with_timer(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        a = time.perf_counter()
+        ret = f(*args, **kwargs)
+        b = time.perf_counter()
+        logger.debug(f"[timer] ({f.__name__}) {b - a:.4e}s")
+        return ret
+
+    return wrapped
