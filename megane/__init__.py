2c29500449c4a7b3fe255f1ad8b597590ddbd8ce
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-15 02:42:19 +0700

    import everything

--- megane/__init__.py
+++ megane/__init__.py
@@ -1,7 +1,6 @@
 from megane import utils
-from megane.configs import ModelConfig, TrainConfig
-from megane.configs import read as read_config
-from megane.data import Sample, get_dataset
-from megane.models import Model
-from megane.predictor import Predictor
-from megane.trainer import Trainer
+from megane.configs import *
+from megane.data import *
+from megane.models import *
+from megane.predictor import *
+from megane.trainer import *
