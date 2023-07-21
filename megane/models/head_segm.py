3b0616d64156da051a32154f554b67c88d17b560
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-21 17:24:38 +0700

    clsm: use traditional segmentation head

--- megane/models/head_segm.py
+++ megane/models/head_segm.py
@@ -36,13 +36,11 @@ def encode_clsm_straight(
     boxes: List[Tuple[int, int, int, int]],
     classes: List[int],
 ):
-    targets = torch.zeros((C + 1, H, W), dtype=torch.float32)
+    targets = torch.zeros((C, H, W), dtype=torch.float32)
     for i, (x1, y1, x2, y2) in enumerate(boxes):
         c = classes[i]
-        targets[c + 1, y1:y2, x1:x2] = 1
+        targets[c, y1:y2, x1:x2] = 1
 
-    targets[0] = (1 - targets[1:].max(dim=0).values) > 0
-    targets = torch.clamp(targets, 0, 1)
     return targets
 
 
@@ -88,7 +86,6 @@ class CLSMDecoder:
         if not ground_truth:
             if isinstance(outputs, (list, tuple)):
                 outputs = outputs[0]
-            outputs = torch.softmax(outputs, dim=-3)
 
         # Decode image
         image = TF.to_pil_image(inputs)
@@ -98,15 +95,13 @@ class CLSMDecoder:
         boxes = []
         classes = []
         for i in range(outputs.shape[-3]):
-            if i == 0:
-                continue
             mask = (outputs[i] * 255).astype("uint8")
             stats = cv2.connectedComponentsWithStats(mask)
             for x, y, w, h, s in stats[2]:
                 box = (x / W, y / H, (x + w) / W, (y + h) / H)
                 box = xyxy2polygon(box)
                 boxes.append(box)
-                classes.append(i - 1)
+                classes.append(i)
         return Sample(image, classes=classes, boxes=boxes)
 
 
@@ -118,9 +113,11 @@ class ClassifierSegmenter(nn.Module):
             nn.Conv2d(in_channels, in_channels // 4, 3, padding=1),
             nn.BatchNorm2d(in_channels // 4),
             nn.ReLU(),
-            nn.ConvTranspose2d(in_channels // 4, num_classes + 1, 4, stride=4),
+            nn.ConvTranspose2d(in_channels // 4, num_classes, 4, stride=4),
         )
-        self.rrm = ResidualRefinementModule(num_classes + 1)
+        self.rrm = nn.Sequential(
+            ResidualRefinementModule(num_classes),
+        )
 
     def forward(self, x, *args, **kwargs):
         x = self.conv_1(x)
@@ -139,18 +136,62 @@ class ClassifierSegmenter(nn.Module):
             loss = loss + F.cross_entropy(pr, gt)
         return loss / 5
 
+    def loss_sampling(self, losses, masks, weights):
+        loss = 0
+        total = 0
+        for mask, weight in zip(masks, weights):
+            if torch.count_nonzero(mask) == 0:
+                continue
+            loss += losses[mask].mean() * weight
+            total += weight
+        loss = loss / total
+        return loss
+
+    def loss_sampling2(self, losses, positives, negatives=None, n_pos=None, n_neg=None):
+        if negatives is None:
+            negatives = ~positives
+        if n_pos is None:
+            n_pos = torch.count_nonzero(positives)
+        if n_neg is None:
+            n_neg = min(torch.count_nonzero(negatives), n_pos * 3)
+        if n_pos == 0 or n_neg == 0:
+            return losses.mean()
+
+        p_losses, _ = torch.topk(losses[positives], n_pos)
+        n_losses, _ = torch.topk(losses[negatives], n_neg)
+        loss = p_losses.mean() + n_losses.mean()
+        return loss
+
     def compute_loss(self, outputs, targets):
         refined, unrefined = outputs
+        unrefined = torch.sigmoid(unrefined)
+        refined = torch.sigmoid(refined)
+        positives = (torch.sigmoid(refined) > 0.75) == targets
+        # positives = targets > 0
+        # tp = (predicts == 1) & (targets == 1)
+        # fp = (predicts == 1) & (targets == 0)
+        # tn = (predicts == 0) & (targets == 0)
+        # fn = (predicts == 0) & (targets == 1)
+        # unsure = (refined <= 0.9) & (refined >= 0.1)
+        negatives = ~positives
+        n_pos = torch.count_nonzero(positives)
+        n_neg = min(torch.count_nonzero(negatives), n_pos * 3)
+
         loss = 0
-        loss += self.ms_ce(refined, targets)
-        loss += self.ms_ce(unrefined, targets)
-        loss += L.dice_ssim_loss(torch.sigmoid(refined), targets)
-        loss += L.dice_ssim_loss(torch.sigmoid(unrefined), targets)
+        bce = F.binary_cross_entropy_with_logits
+        losses = bce(unrefined, targets, reduction="none")
+        loss += self.loss_sampling2(losses, positives, negatives, n_pos, n_neg)
+        losses = bce(refined, targets, reduction="none")
+        loss += self.loss_sampling2(losses, positives, negatives, n_pos, n_neg)
+        losses = L.dice_ssim_loss(refined, targets, reduction="none")
+        loss += self.loss_sampling2(losses, positives, negatives, n_pos, n_neg)
+        losses = L.dice_ssim_loss(unrefined, targets, reduction="none")
+        loss += self.loss_sampling2(losses, positives, negatives, n_pos, n_neg)
         return loss / 4
 
     def visualize_outputs(self, outputs, logger, tag, step, ground_truth=False):
         if not ground_truth:
-            outputs = torch.sigmoid(outputs[0] * 50)
+            outputs = torch.sigmoid(outputs[0])
         outputs = torch.cat(list(outputs), dim=-2)
         outputs = torch.cat(list(outputs), dim=-1)
         outputs = outputs.unsqueeze(0)
